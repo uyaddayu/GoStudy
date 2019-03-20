51a1f9a556b9622e88e3747c47382205bb3239f0
18125953381 <952479221@qq.com>
2019-03-20 13:38:07 +0800

    hello world3

--- learn_02.go
+++ learn_02.go
@@ -5,17 +5,17 @@ import (
 	"strings"
 )
 
-//简单的字符串
+// 简单的字符串
 func main() {
 	str1 := "Go"
 	str2 := "go"
 	fmt.Println(str1, str2)
-	//将小写转成大写
+	// 将小写转成大写
 	str2 = strings.ToUpper(str2)
-	//将大写转成小写
+	// 将大写转成小写
 	str1 = strings.ToLower(str1)
 	fmt.Println(str1, str2)
-	//判断是否相等
+	// 判断是否相等
 	fmt.Println(strings.EqualFold(str1, str2))
 	fmt.Println(str1 == str2)
 }
