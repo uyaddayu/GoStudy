3bba633020d3f00046af5548d719cd1e2b5e4613
18125953381 <952479221@qq.com>
2019-03-20 13:36:53 +0800

    hello world2

---
+++ learn_02.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+//简单的字符串
+func main() {
+	str1 := "Go"
+	str2 := "go"
+	fmt.Println(str1, str2)
+	//将小写转成大写
+	str2 = strings.ToUpper(str2)
+	//将大写转成小写
+	str1 = strings.ToLower(str1)
+	fmt.Println(str1, str2)
+	//判断是否相等
+	fmt.Println(strings.EqualFold(str1, str2))
+	fmt.Println(str1 == str2)
+}
